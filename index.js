587dd9317864112539b007d228cf5dbf983cae64
li_mei_chao <957779152@qq.com>
2021-01-13 17:51:56 +0800

    第一版

--- index.js
+++ index.js
@@ -3,16 +3,25 @@ const {program} = require('commander');
 //--help 提示
 const {helpOptions} = require('./lib/core/help')
 //create指令
-const {createCommands} = require('./lib/core/create')
+const {createCommands,addComponent,addPage,request,api, demo} = require('./lib/core/create');
 //版本号
-program.version(require('./package.json').version, '-v,--version');
+program.version(require('./package.json').version, '-v,--version','version(版本号)');
 
 //增加自己的options
 helpOptions()
 
 //创建其他指令
+
+//创建项目
 createCommands()
-
+//创建组件
+addComponent()
+//创建页面
+addPage()
+//生成request
+request()
+//生成api
+api()
+//demo
+demo()
 program.parse(process.argv)
-
-console.log(program.dest);
